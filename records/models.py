396b66fff22445bcecaeecc70fdfb042a8cc9221
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-02-17 17:23:59 +0000

    add records app and genre and record model, registered models in admin

---
+++ records/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    """
+    This is the Genre model, to add a record to the store
+    where the genre doesnt already exist, the admin will
+    need to add the genre first prior to adding the record.
+    """
+    genre = models.CharField(max_length=254)
+
+    def __str__(self):
+        return str(self.genre)
+
+
+class Record(models.Model):
+    """
+    This is the record model, it sets the db table according to
+    the data required to show a new record in the store.
+    """
+    CONDITION_CHOICES = (
+        ('mint', 'M'),
+        ('near_mint', 'NM'),
+        ('very_good', 'VG'),
+        ('good', 'G'),
+        ('fair', 'F'),
+        ('poor', 'P'),
+    )
+    genre = models.ForeignKey(
+        'Genre', null=True, blank=False, on_delete=models.SET_NULL
+        )
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=254, null=False, blank=False)
+    slug = models.SlugField(max_length=254, null=False, blank=False)
+    artist = models.CharField(max_length=254, null=False, blank=False)
+    record_label = models.CharField(max_length=254, null=False, blank=False)
+    release_year = models.CharField(max_length=4, null=False, blank=False)
+    hot_pick = models.BooleanField(default=False)
+    condition = models.CharField(
+        max_length=9, choices=CONDITION_CHOICES,
+        default='mint', blank=False, null=False
+        )
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    tracklist = models.TextField()
+    description = models.TextField()
+    has_link = models.BooleanField(default=False)
+    link_to_music = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.title)
--- wax_crate/settings.py
+++ wax_crate/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'records',
 
     # Other apps
     'storages',
