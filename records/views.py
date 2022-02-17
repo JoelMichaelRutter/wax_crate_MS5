199029e9280a276718b205c64b7b15f22149874a
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-02-17 18:38:43 +0000

    add records template initial styling and containers, add style to style.css

--- records/views.py
+++ records/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Record
 
-# Create your views here.
+
+def all_records(request):
+    """
+    This view shows all the records in the shop.
+    """
+    records = Record.objects.all()
+    context = {
+        'records': records,
+    }
+    return render(request, 'records/records.html', context)
