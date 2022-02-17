8952d1db3e0f7b1f3fbbd117cb46b2231c8ee37c
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-02-17 17:46:33 +0000

    customised admin in records app

--- records/admin.py
+++ records/admin.py
@@ -2,5 +2,39 @@ from django.contrib import admin
 from .models import Genre, Record
 
 # Register your models here.
-admin.site.register(Genre)
-admin.site.register(Record)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    """
+    This admin class manages the genre model within
+    the django admin.
+    """
+    list_display = [
+        'genre'
+    ]
+
+
+class RecordAdmin(admin.ModelAdmin):
+    """
+    This admin class manages the record model within
+    the django admin.
+    """
+    list_display = [
+        'id',
+        'artist',
+        'title',
+        'record_label',
+        'release_year',
+        'hot_pick',
+        'condition',
+        'price',
+        'has_link',
+    ]
+
+    ordering = ['artist']
+
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Record, RecordAdmin)
