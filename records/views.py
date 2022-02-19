bacdce08a1c39acb8d46fb703adeac0bf32a147d
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-02-19 17:39:01 +0000

    add record detail base functionality, template, view and url, tidied up some teething problems with base template layout

--- records/views.py
+++ records/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Record
 
 
@@ -11,3 +11,18 @@ def all_records(request):
         'records': records,
     }
     return render(request, 'records/records.html', context)
+
+
+def show_record_details(request, slug):
+    """
+    This view shows the detail for the paticular record
+    selected by the user. It takes the slug which is generated
+    from the title of of the record and uses that to locate the
+    correct database entry. It is then inseted as context into
+    the record_details template and is also used within the URL.
+    """
+    record = get_object_or_404(Record, slug=slug)
+    context = {
+        'record': record,
+    }
+    return render(request, 'records/record_details.html', context)
