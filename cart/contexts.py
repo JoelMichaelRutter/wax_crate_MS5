b5b12d04e97fdd3121531b172cb8477087c0c151
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-02-23 22:05:20 +0000

    fix bugs and problems within cart contexts.py file

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,3 +1,7 @@
+"""
+1 - Importing Decimal for use in calculating delivery charges
+2 - Importing settings so that delivery settings can be accessed.
+"""
 from decimal import Decimal
 from django.conf import settings
 
@@ -13,20 +17,22 @@ def cart_items(request):
 
     if total < settings.FREE_DELIVERY_ON_ORDERS_OVER:
         delivery_charge = Decimal(settings.STANDARD_DELIVERY_COST)
-        free_delivery_difference = settings.FREE_DELIVERY_ON_ORDERS_OVER - total
+        free_delivery_difference = (
+            settings.FREE_DELIVERY_ON_ORDERS_OVER - total
+            )
     else:
         delivery_charge = 0
         free_delivery_difference = 0
-    
-    order_total = delivery_charge + total 
-    
+
+    order_total = delivery_charge + total
+
     context = {
         'records_in_cart': records_in_cart,
         'total': total,
         'record_count': record_count,
         'delivery_charge': delivery_charge,
-        'free_delivery_difference', free_delivery_difference,
-        'FREE_DELIVERY_ON_ORDERS_OVER': FREE_DELIVERY_ON_ORDERS_OVER,
+        'free_delivery_difference': free_delivery_difference,
+        'free_deliery_on_orders_over': settings.FREE_DELIVERY_ON_ORDERS_OVER,
         'order_total': order_total,
     }
 
