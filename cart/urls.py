9cb81e370adc21f19dde486c0f5ef8bc76936790
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-02-26 14:38:03 +0000

    add amend and remove views to cart views.py file, finalised js to handle submission of aty form in cart.html. update and remove functionality complete

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.show_cart, name='show_cart'),
-    path('add/<record_id>/', views.add_record_to_cart, name='add_record_to_cart'),
+    path(
+        'add/<record_id>/',
+        views.add_record_to_cart,
+        name='add_record_to_cart'
+        ),
+    path('amend/<record_id>/', views.amend_cart, name='amend_cart'),
+    path('remove/<record_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def show_cart(request):
@@ -25,4 +25,46 @@ def add_record_to_cart(request, record_id):
         cart[record_id] = quantity
 
     request.session['cart'] = cart
-    return (redirect(redirect_url))
+    return redirect(redirect_url)
+
+
+def amend_cart(request, record_id):
+    """
+    This view handles amending the quantity of
+    records in the cart prior to checkout.
+    """
+    # Get quantity from post data from cart-qty control field.
+    quantity = int(request.POST.get('quantity'))
+    # Set cart to the current cart in the browser session.
+    cart = request.session.get('cart', {})
+    # Check if qty is geater than 0 and if true
+    # set quantity of record id in cart to new quantity from
+    # form.
+    if quantity > 0:
+        cart[record_id] = quantity
+    # If quantity is not greater than 0, pop method removes
+    # the record from the bag.
+    else:
+        cart.pop(record_id)
+    # Updates the cart in the session so that the user
+    # sees the new cart.
+    request.session['cart'] = cart
+    # Redirects the user back to the show cart which
+    # is now updated.
+    return redirect(reverse('show_cart'))
+
+
+def remove_from_cart(request, record_id):
+    """
+    This view handles deleting an item from the cart
+    by using the delete button.
+    """
+
+    cart = request.session.get('cart', {})
+
+    try:
+        cart.pop(record_id)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
