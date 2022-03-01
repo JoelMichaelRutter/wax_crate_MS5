87131e01bcb0ba7ec7b1200045cfbedeec69c200
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-03-01 16:39:34 +0000

    register checkout models in checkout admin and create signals file to update relevant totals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import Order, LinesInOrder
 
-# Register your models here.
+class LinesInOrderAdminInline(admin.TabularInline):
+    model = LinesInOrder
+    readonly_fields = ('line_total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Class to handle order table within the
+    admin site.
+    """
+    inlines = (LinesInOrderAdminInline,)
+    readonly_fields = (
+        'order_number',
+        'order_date',
+        'delivery_charge',
+        'purchase_total_cost',
+        'order_total',
+    )
+
+    fields = (
+        'order_number',
+        'order_date',
+        'customer_full_name',
+        'customer_email',
+        'customer_street_address1',
+        'customer_street_address2',
+        'customer_town_or_city',
+        'customer_postcode',
+        'customer_county',
+        'purchase_total_cost',
+        'delivery_charge',
+        'order_total',
+    )
+
+    list_display = (
+        'order_number',
+        'order_date',
+        'customer_full_name',
+        'customer_email',
+        'customer_street_address1',
+        'customer_town_or_city',
+        'customer_postcode',
+        'purchase_total_cost',
+        'delivery_charge',
+        'order_total',
+    )
+
+    ordering = ('-order_date',)
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/signals.py
@@ -0,0 +1,42 @@
+"""
+1 - Importing post_save and post_delete signals.
+2 - Importing the receiver to receive signals from models.
+3 - Importing the LinesInOrder model as this model is responsible
+    for sending the signals.
+"""
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import LinesInOrder
+
+
+@receiver(post_save, sender=LinesInOrder)
+def update_order_lines_on_save(sender, instance, created, **kwargs):
+    """
+    This function updates the lines within the order model.
+    sender - This is the paticular model that the signal is sent from.
+    In this case the sender is the LinesInOrder model.
+    instance - The specific instance of the LinesInOrder model that sent
+    the signal.
+    created - This takes a boolean peice of data to understand whether
+    the instance is a brand new row of data or an update to an existing
+    row.
+    **kwargs - any additional key word arguments.
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=LinesInOrder)
+def update_order_lines_on_delete(sender, instance, **kwargs):
+    """
+    This function updates the lines within the order model.
+    sender - This is the paticular model that the signal is sent from.
+    In this case the sender is the LinesInOrder model.
+    instance - The specific instance of the LinesInOrder model that sent
+    the signal.
+    created - This takes a boolean peice of data to understand whether
+    the instance is a brand new row of data or an update to an existing
+    row.
+    **kwargs - any additional key word arguments.
+    """
+    instance.order.update_total()
