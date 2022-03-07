b0cde0470856b9ab569529fc0c702003fbc8de83
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-03-07 23:51:38 +0000

    add accounts template, css file and debug urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_customer_account(request):
+    """
+    Show the customers account form.
+    """
+    template = 'accounts/account.html'
+
+    context = {
+
+    }
+
+    return render(request, template, context)
--- wax_crate/urls.py
+++ wax_crate/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('records/', include('records.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('account/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
