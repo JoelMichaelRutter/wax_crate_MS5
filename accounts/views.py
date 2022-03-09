3d35f834fd86884c1d10be9625355d4e771e2ea1
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-03-09 09:48:45 +0000

    updated accounts views.py file to get customer account from the CustomerAccount model

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import CustomerAccount
 
 def show_customer_account(request):
     """
     Show the customers account form.
     """
+    customer_account = get_object_or_404(
+        CustomerAccount, user=request.user
+    )
+
     template = 'accounts/account.html'
 
     context = {
-
+        'customer_account': customer_account,
     }
 
     return render(request, template, context)
