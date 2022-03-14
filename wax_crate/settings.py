895562d1713c2756bfa359e952920b74a2f2d11d
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-03-14 21:18:20 +0000

    updated settings.py in project level directory with production email settings

--- wax_crate/settings.py
+++ wax_crate/settings.py
@@ -113,8 +113,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -201,8 +199,7 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
-DEFAULT_FROM_EMAIL = 'waxcrate_recordshop@example.co.uk'
-
+# AWS Storage configuration (applicable only on deployed site.)
 if 'USE_AWS' in os.environ:
     # File caching control settings
     AWS_S3_OBJECT_PARAMETERS = {
@@ -265,3 +262,17 @@ SUMMERNOTE_CONFIG = {
         ],
     }
 }
+
+# Deployed email settings
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'waxcrate_recordshop@example.co.uk'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
